31b4b38c773a676be1f31a616393d6c4810292a6
Andy Luu <aluu5675@uw.edu>
2020-06-05 21:26:50 -0700

    YelpService added

---
+++ app/src/main/java/com/matthewputra/hungrygames/model/YelpService.kt
@@ -0,0 +1,15 @@
+package com.matthewputra.hungrygames.model
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+public interface YelpService {
+
+    @GET("businesses/search")
+    fun searchRestaurants(
+        @Header("authorization") authHeader: String,
+        @Query("term") searchTerm: String,
+        @Query("location") location: String): Call<Any>
+}
